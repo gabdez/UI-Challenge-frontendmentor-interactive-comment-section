2b0d471434e245ffeb20ee7b80d1ac7eea0a6aa3
FERNANDEZ CASA Gabriel 00050553 <gabriel.fernandezcasa@3ds.com>
2024-03-31 01:04:42 +0100

    setup project

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <v-app>
+    <v-layout>
+      <v-main>
+        Main Content
+      </v-main>
+    </v-layout>
+  </v-app>
+</template>
+
+<style scoped></style>
+
+<script setup lang="ts">
+</script>
